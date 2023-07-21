1b9d972f24f941ab8754538d0fd4cc86fc35cf17
AlcoholMints <cgiles00820@gmail.com>
2023-07-21 06:50:33 -0500

    Add song to playlist using refresh tokens

--- main.py
+++ main.py
@@ -16,30 +16,70 @@ def get_access_token():
     # Replace with your Spotify API credentials
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    REDIRECT_URI = "http://localhost:8888/callback"
+    REDIRECT_URI = "http://localhost/"  # Replace with your redirect URI
     SCOPES = "playlist-modify-public"  # Required scope for modifying a public playlist
 
-    auth_url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
-    print("Please visit the following URL to authorize the application:")
-    print(auth_url)
+    # Check if a refresh token is already available
+    try:
+        with open("refresh_token.txt", "r") as file:
+            refresh_token = file.read().strip()
+    except FileNotFoundError:
+        refresh_token = None
 
-    # After authorizing, the Spotify Accounts service will redirect you back to the specified Redirect URI
-    auth_code = input("Enter the authorization code from the redirected URL: ")
+    if not refresh_token:
+        # Generate the authorization URL
+        auth_url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
+        print("Please visit the following URL to authorize the application:")
+        print(auth_url)
 
+        # Get the authorization code from the user
+        auth_code = input("Enter the authorization code from the redirected URL: ")
+
+        # Exchange the authorization code for access and refresh tokens
+        token_url = "https://accounts.spotify.com/api/token"
+        data = {
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": REDIRECT_URI,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        }
+
+        response = requests.post(token_url, data=data)
+        if response.status_code == 200:
+            token_data = response.json()
+            access_token = token_data["access_token"]
+            refresh_token = token_data["refresh_token"]
+
+            # Save the refresh token for future use
+            with open("refresh_token.txt", "w") as file:
+                file.write(refresh_token)
+        else:
+            print("Failed to obtain access and refresh tokens.")
+            return None
+
+    else:
+        access_token = refresh_access_token(CLIENT_ID, CLIENT_SECRET, refresh_token)
+
+    return access_token
+
+def refresh_access_token(client_id, client_secret, refresh_token):
+    # Use the refresh token to get a new access token
     token_url = "https://accounts.spotify.com/api/token"
     data = {
-        "grant_type": "authorization_code",
-        "code": auth_code,
-        "redirect_uri": REDIRECT_URI,
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "client_secret": client_secret,
     }
 
     response = requests.post(token_url, data=data)
     if response.status_code == 200:
-        return response.json()["access_token"]
+        token_data = response.json()
+        access_token = token_data["access_token"]
+        return access_token
     else:
-        print("Failed to obtain access token.")
+        print("Failed to refresh access token.")
         return None
 
 def add_song_to_playlist(access_token, playlist_id, track_uri):
@@ -62,7 +102,7 @@ def main():
     access_token = get_access_token()
     if access_token:
         playlist_id = os.getenv('YOUR_PLAYLIST_ID')  # Replace with your playlist ID
-        track_uri = "spotify:track:3KkXRkHbMCARz0aVfEt68P"  # URI for "I Will Always Love You" by Whitney Houston
+        track_uri = "spotify:track:4eHbdreAnSOrDDsFfc4Fpm"  # URI for "I Will Always Love You" by Whitney Houston
 
         add_song_to_playlist(access_token, playlist_id, track_uri)
 
