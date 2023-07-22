33645fa52ad6a282d234c3845ff26ce653bd451e
AlcoholMints <cgiles00820@gmail.com>
2023-07-22 06:23:34 -0500

    Converted to Spotify track ids. moved functions.

---
+++ get_spotify_track_id.py
@@ -0,0 +1,74 @@
+# datagrabber function
+# * Function that grabs all the song and artists played on 95.9 that day in a list
+# ? Then it converts to a list of spotify track ids?
+
+# import libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from tokens import get_access_token
+
+def data_grabber():
+    # Create a URL object
+    url = 'https://onlineradiobox.com/us/kfwr/playlist/?cs=us.kfwr'
+
+    # Create object page
+    page = requests.get(url)
+
+    # parser-lxml - Change html to a Python friendly format
+    # obtain the page's information
+    soup = BeautifulSoup(page.text, 'lxml')
+    # grab the table on the front, the songs played that day
+    song_table = soup.find('table')
+    # pull all "a" from the table and put into another list of songs
+    song_list = []
+    for i in song_table.find_all('a'):
+        title = i.text
+        song_list.append(title)
+    return song_list
+
+def get_spotify_track_id():
+    # Get the song list and the access token
+    song_list = data_grabber()
+    access_token = get_access_token()
+
+    base_url = 'https://api.spotify.com/v1/search'
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    track_ids = []
+    for song_artist in song_list:
+        # Split the song_artist string into artist and song parts
+        artist_song = song_artist.split(' - ')
+
+        # Check if the artist and song are provided in the correct format
+        if len(artist_song) != 2:
+            print(f"Invalid format for song_artist: {song_artist}")
+            continue
+
+        artist, song = artist_song
+        query = f'{artist} {song}'
+
+        # Make the API request to search for the track
+        params = {
+            'q': query,
+            'type': 'track',
+            'limit': 1
+        }
+        response = requests.get(base_url, headers=headers, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            tracks = data.get('tracks', {}).get('items', [])
+            if tracks:
+                track_ids.append(tracks[0]['id'])
+            else:
+                print(f"Track not found for: {query}")
+        else:
+            print(f"Error fetching data for: {query}")
+            
+    print(track_ids)
+    return track_ids
+
+if __name__ == '__main__':
+    get_spotify_track_id()
--- main.py
+++ main.py
@@ -3,106 +3,38 @@
 # What redirect uri should I use if I were running this on Heroku?
 # What are scopes and where can I find a list of them?
     # Again, I can ask this later.
+# ! Re-ask the request and tell it that it needs to add multiple songs. I think we need a for loop. May be able to make it myself.
 
 # import libraries
 import requests
 import os
-from dotenv import load_dotenv
+from tokens import get_access_token
+from get_spotify_track_id import get_spotify_track_id
 
-#
-def get_access_token():
-    # Define all of the clint IDs
-    CLIENT_ID = os.getenv('CLIENT_ID')
-    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    REDIRECT_URI = "http://localhost/"  # may need to replace for heroku. this needs to be updated in spotify as well.
-    SCOPES = "playlist-modify-public"  # Required scope for modifying a public playlist
 
-    # Check if a refresh token is already available
-    try:
-        with open("refresh_token.txt", "r") as file:
-            refresh_token = file.read().strip()
-    except FileNotFoundError:
-        refresh_token = None
-
-    if not refresh_token:
-        # Generate the authorization URL
-        auth_url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
-        print("Please visit the following URL to authorize the application:")
-        print(auth_url)
-
-        # Get the authorization code from the user
-        auth_code = input("Enter the authorization code from the redirected URL: ")
-
-        # Exchange the authorization code for access and refresh tokens
-        token_url = "https://accounts.spotify.com/api/token"
-        data = {
-            "grant_type": "authorization_code",
-            "code": auth_code,
-            "redirect_uri": REDIRECT_URI,
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-        }
-
-        response = requests.post(token_url, data=data)
-        if response.status_code == 200:
-            token_data = response.json()
-            access_token = token_data["access_token"]
-            refresh_token = token_data["refresh_token"]
-
-            # Save the refresh token for future use
-            with open("refresh_token.txt", "w") as file:
-                file.write(refresh_token)
-        else:
-            print("Failed to obtain access and refresh tokens.")
-            return None
-
-    else:
-        access_token = refresh_access_token(CLIENT_ID, CLIENT_SECRET, refresh_token)
-
-    return access_token
-
-def refresh_access_token(client_id, client_secret, refresh_token):
-    # Use the refresh token to get a new access token
-    token_url = "https://accounts.spotify.com/api/token"
-    data = {
-        "grant_type": "refresh_token",
-        "refresh_token": refresh_token,
-        "client_id": client_id,
-        "client_secret": client_secret,
-    }
-
-    response = requests.post(token_url, data=data)
-    if response.status_code == 200:
-        token_data = response.json()
-        access_token = token_data["access_token"]
-        return access_token
-    else:
-        print("Failed to refresh access token.")
-        return None
-
-def add_song_to_playlist(access_token, playlist_id, track_uri):
+def add_songs_to_playlist(access_token, playlist_id, track_uris):
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json",
     }
     data = {
-        "uris": [track_uri],
+        "uris": [track_uris],
     }
     endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
     response = requests.post(endpoint, headers=headers, json=data)
     if response.status_code == 201:
-        print("Song added to the playlist successfully!")
+        print("Songs added to the playlist successfully!")
     else:
         print("Failed to add the song to the playlist.")
 
 def main():
     access_token = get_access_token()
     if access_token:
-        playlist_id = os.getenv('YOUR_PLAYLIST_ID')  # Replace with your playlist ID
-        track_uri = "spotify:track:4eHbdreAnSOrDDsFfc4Fpm"  # URI for "I Will Always Love You" by Whitney Houston
+        playlist_id = os.getenv('YOUR_PLAYLIST_ID') 
+        spotify_track_ids = get_spotify_track_id()
 
-        add_song_to_playlist(access_token, playlist_id, track_uri)
+        add_songs_to_playlist(access_token, playlist_id, spotify_track_ids)
 
 if __name__ == "__main__":
     main()
